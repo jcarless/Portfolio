22b2f904d916bd6b2711a6fb9ff22899aab58da4
Jerome Carless <carless.jerome@gmail.com>
2016-05-07 23:12:24 -0400

    profile updates

---
+++ app/routing/html-routes.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = function(app){
+
+	app.get('/index', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+
+	});
+
+	app.get('/coderswholift', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/fitness.html'));
+
+	});
+
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+}
