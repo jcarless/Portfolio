41db7304d9bf69b896ad49de545af69768c08d4a
jcarless <carless.jerome@gmail.com>
2016-06-04 09:59:20 -0400

    added jewelry box

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,11 +7,6 @@ module.exports = function(app){
 
 	});
 
-	app.get('/coderswholift', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/fitness.html'));
-
-	});
-
 	app.use(function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/index.html'));
 	});
